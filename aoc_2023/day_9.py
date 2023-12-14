f0d1e8e2e2b32e5a92cb9cc07343b374eb270b6b
Aditya Badrinath <abadrinath15@gmail.com>
2023-12-14 08:33:50 +0530

    day 9 part 2: need to debug

--- aoc_2023/day_9.py
+++ aoc_2023/day_9.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 import pandas
+import typing
+import functools
 
 
 def extrapolate_history(history: "pandas.Series[float]") -> int:
@@ -11,19 +13,34 @@ def extrapolate_history(history: "pandas.Series[float]") -> int:
     return int(sum(x.iat[-1] for x in reduced_histories))
 
 
-def sum_extrapolated_histories(fp: str) -> int:
+def extrapolate_history_front(history: "pandas.Series[float]") -> int:
+    reduced_histories: "list[pandas.Series[float]]" = [history]
+    while (history != 0).any():
+        history = history.diff().dropna()
+        reduced_histories.append(history)
+
+    return int(
+        functools.reduce(lambda x, y: y - x, (x.iat[0] for x in reduced_histories))
+    )
+
+
+def sum_extrapolated_histories(
+    fp: str, extrapolation_function: typing.Callable[["pandas.Series[float]"], int]
+) -> int:
     total = 0
     with open(fp) as file:
         for line in file:
             history_str = line.split()
             history = pandas.Series(history_str).astype("float")
-            total += extrapolate_history(history)
+            total += extrapolation_function(history)
 
     return total
 
 
 def main() -> None:
-    print(sum_extrapolated_histories("aoc_2023/inputs/day_9.txt"))
+    fp = "aoc_2023/inputs/day_9.txt"
+    print(sum_extrapolated_histories(fp, extrapolate_history))
+    print(sum_extrapolated_histories(fp, extrapolate_history_front))
 
 
 if __name__ == "__main__":
--- tests/test_day_9.py
+++ tests/test_day_9.py
@@ -13,3 +13,15 @@ import pandas
 def test_extrapolate_history(history: "pandas.Series[float]", exp: int) -> None:
     res = day_9.extrapolate_history(history)
     assert res == exp
+
+
+def test_extrapolate_history_front() -> None:
+    history = pandas.Series([10, 13, 16, 21, 30, 45])
+    res = day_9.extrapolate_history_front(history)
+    assert res == 5
+
+
+def test_sum_extrapolated_histories() -> None:
+    fp = "aoc_2023/examples/day_9.txt"
+    res = day_9.sum_extrapolated_histories(fp, day_9.extrapolate_history_front)
+    assert res == 2
